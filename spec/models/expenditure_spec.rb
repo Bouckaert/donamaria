192fe84f7e40983bb0ec81a118b67cd6914295a9
embs <embs@cin.ufpe.br>
2014-11-28 22:13:13 -0300

    Introduz specs

---
+++ spec/models/expenditure_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Expenditure do
+  it { should respond_to :sequencial_candidato}
+  it { should respond_to :seller}
+  it { should respond_to :price}
+  it { should respond_to :description}
+  it { should respond_to :candidature}
+  it { should belong_to :candidature }
+end

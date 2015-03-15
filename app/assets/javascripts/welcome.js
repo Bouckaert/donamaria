08aa2d687155349adb8eb333f8054078c5efbba1
embs <embs@cin.ufpe.br>
2015-03-15 17:24:13 -0300

    Reintroduz versão antiga do bootstrap para consertar estilos da landing, que dependem dessa versão

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -14,7 +14,4 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require turbolinks
-//= require jquery.scrollTo-1.4.2-min
-//= require jquery.localscroll-1.2.7-min
-//= require jquery.prettyPhoto
-//= require site
+//= require smooth_scroll

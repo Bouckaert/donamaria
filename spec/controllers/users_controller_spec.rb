cfea9513d68f24382c1d9f631ceedb76997f88fa
embs <embs@cin.ufpe.br>
2015-05-26 17:31:59 -0300

    Fix spec de UsersController

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  let(:user) { FactoryGirl.create :user }
 
   describe "GET show" do
     it "returns http success" do
-      get :show
+      get :show, id: user
       expect(response).to have_http_status(:success)
     end
   end

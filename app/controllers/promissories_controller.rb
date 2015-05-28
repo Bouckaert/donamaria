f8d04ca99a5f3958fd266116b3db9f670bff5301
embs <embs@cin.ufpe.br>
2015-05-27 21:04:48 -0300

    Refatora promissories#create
    Quebra a action, que estava gigantesca, em duas: a própria #create
    e #preview.

--- app/controllers/promissories_controller.rb
+++ app/controllers/promissories_controller.rb
@@ -8,48 +8,46 @@ class PromissoriesController < ApplicationController
     @representative = @proposal.representative
   end
 
+  def preview
+    @current_month = Date.today + 1.month
+    @current_month = @current_month.strftime("%B")
+    @current_year = Time.now.year
+    @promissory = Promissory.new(promissory_params)
+    @promissory.user = current_user
+    @proposal = Proposal.find(params[:proposal_id])
+    @promissory.proposal = @proposal
+    @representative = Representative.find(@proposal.representative_id)
+    if @promissory.valid?
+      flash[:success] = "Confirme os dados da promissória"
+    else
+      redirect_to @promissory.user
+      flash[:error] = @promissory.errors.full_messages_for(@promissory.errors.first.first)
+    end
+  end
+
   def create
-    if params[:commit] == "Avançar"
-      @current_month = Date.today + 1.month
-      @current_month = @current_month.strftime("%B")
-      @current_year = Time.now.year
+    if user_signed_in?
       @promissory = Promissory.new(promissory_params)
       @promissory.user = current_user
       @proposal = Proposal.find(params[:proposal_id])
       @promissory.proposal = @proposal
-      @representative = Representative.find(@proposal.representative_id)
-      if @promissory.valid?
-        render :preview
-        flash[:success] = "Confirme os dados da promissória"
+      @representative = @proposal.representative_id
+      if @promissory.save
+        flash[:success] = "Promissória cadastrada com sucesso!"
       else
-        redirect_to @promissory.user
         flash[:error] = @promissory.errors.full_messages_for(@promissory.errors.first.first)
       end
+      redirect_to @promissory.user
     else
-      if user_signed_in?
-        @promissory = Promissory.new(promissory_params)
-        @promissory.user = current_user
-        @proposal = Proposal.find(params[:proposal_id])
-        @promissory.proposal = @proposal
-        @representative = @proposal.representative_id
-        if @promissory.save
-          flash[:success] = "Promissoria cadastrada com sucesso!"
-        else
-          flash[:error] = @promissory.errors.full_messages_for(@promissory.errors.first.first)
-        end
-        redirect_to @promissory.user
-      else
-        redirect_to '/users/sign_in'
-        flash[:alert] = 'Favor efetuar login'
-      end
+      redirect_to '/users/sign_in'
+      flash[:alert] = 'Favor efetuar login'
     end
-
   end
 
   private
 
-    def promissory_params
-      params.require(:promissory).permit(:installment, :amount, :expiration_day, :proposal_id, :user_id)
-    end
-
+  def promissory_params
+    params.require(:promissory)
+      .permit(:installment, :amount, :expiration_day, :proposal_id, :user_id)
+  end
 end

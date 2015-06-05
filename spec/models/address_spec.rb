af97b7ff0a6bf37297ae1d46e074358f26ad9332
embs <embs@cin.ufpe.br>
2015-06-05 13:52:48 -0300

    Adiciona testes que faltavam
    Specs para
    - UsersController;
    - Address;
    - User;

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -2,5 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Address, type: :model do
   it { should validate_presence_of :user }
+  it { should validate_presence_of :zip_code }
   it { should belong_to :user }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 describe User do
   it { should have_many(:promissories) }
   it { should have_many(:proposals).through(:promissories) }
+  it { should accept_nested_attributes_for(:address) }
 end
